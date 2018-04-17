72e29d617b4594d7bb3f6bcfcfe5cedc5615ef3b
Haonan <hhaonan@outlook.com>
2018-04-17 09:36:55 -0400

    Update App.js
    Remove useless lines

--- my-app/App.js
+++ my-app/App.js
@@ -23,38 +23,3 @@ export default StackNavigator({
   }
 );
 
-
-class App extends React.Component {
-
-  render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return ( 
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          {Platform.OS === 'android' && <View style={styles.statusBarUnderlay} />}
-          <RootNavigation />
-        </View> 
-      );  
-    }
-  }
-
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  statusBarUnderlay: {
-    height: 24,
-    backgroundColor: 'rgba(0,0,0,0.2)',
-  },
-});
