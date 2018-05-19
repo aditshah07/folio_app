e9d1ebf84ba3b3b7812ccb337c2e4b40cabdb390
Haonan <hhaonan@outlook.com>
2018-05-18 23:13:37 -0400

    Add files via upload

--- my-app/App.test.js
+++ my-app/App.test.js
@@ -1,25 +1,97 @@
 import React from 'react';
 import App from './App';
-
 import renderer from 'react-test-renderer';
 import Login from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
-it('renders without crashing', () => {
+it('renders App without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
+
   expect(rendered).toBeTruthy();
 });
 
-it('check', () => {
+it('renders Login without crashing', () => {
+  const rendered = renderer.create(<Login />).toJSON();
+
+  expect(rendered).toBeTruthy();
+});
+
+it('check initially isLoggedIn is false', () => {
 	const component = renderer.create(
-		<Login />,
+		<App />
+	);
+	expect(component.getInstance().state.nav.routes[0].params.isLoggedIn).toBe(false)
+});
+
+it('check initially token is empty', () => {
+	const component = renderer.create(
+		<App />
+	);
+	expect(component.getInstance().state.nav.routes[0].params.token).toBe('')
+});
+
+it('check isLoggedIn is true', async() => {
+	const component = renderer.create(
+		<App />
+	);
+	const rendered = renderer.create(
+		<Login />
 	);
 
-	component.getInstance().setState ({
-		userName : 'diku_admin',
-		password : 'admin'
-	})
-	var a = await component.getInstance().onPressCallback();
-	a.then(() => {
-		console.log(this.component.getInstance());
-	},this);
-})
+	async function login () {
+		rendered.getInstance().userName = "diku_admin";
+		rendered.getInstance().password = "admin";
+		var data = await (rendered.getInstance().onPressCallback());
+	}
+	var data  = await login();
+
+	expect(rendered.getInstance().userName).toBe('diku_admin')
+
+	expect(rendered.getInstance().password).toBe('admin')
+
+});
+
+it('print settings info', () => {
+	const component = renderer.create(
+		<App />
+	);
+	console.log(component.getInstance().state.nav.routes[0].params.resources);
+});
+
+it('put input in login page', () => {
+
+	const rendered = renderer.create(
+		<Login />
+	);
+	rendered.getInstance().userName = "diku_admin";
+	rendered.getInstance().password = "admin";
+	
+	expect(rendered.getInstance().userName).toBe('diku_admin')
+
+	expect(rendered.getInstance().password).toBe('admin')
+
+});
+
+it('Invalid password should not login', async() => {
+	const component = renderer.create(
+		<App />
+	);
+	const rendered = renderer.create(
+		<Login />
+	);
+
+	async function login () {
+		rendered.getInstance().userName = "diku_admin";
+		rendered.getInstance().password = "admi";
+		try { 
+			await (rendered.getInstance().onPressCallback());
+		} catch(e){
+			expect(e).toEqual(e);
+		}
+	}
+	var data  = await login();
+
+	expect(component.getInstance().state.nav.routes[0].params.isLoggedIn).toBe(false)
+
+});
---
+++ my-app/LoginScreen.js
@@ -0,0 +1,148 @@
+import React, { Component } from 'react';
+import {
+  Alert,
+  ToolbarAndroid,
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+  Platform,
+  StatusBar,
+} from 'react-native';
+import axios from 'axios';
+import { StackNavigator } from 'react-navigation';
+
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import LoginButton from './lib/LoginButton';
+import HomeScreen from './HomeScreen';
+import RootNavigation from '../navigation/RootNavigation';
+import resources from './data/resources.json';
+import languages from './data/languages.json';
+import hostUrl from './data/url.json'
+
+
+export default class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        this.userName = '';
+        this.password = '';
+    }
+
+    // when press login, post username and password to folio, then get an unique token
+    onPressCallback = () => {
+        const data = JSON.stringify({
+            username: this.userName,
+            password: this.password,
+        });
+        const url = hostUrl.url + '/authn/login';
+        console.log(url);
+        // post username and password
+        axios.post(url, data, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Okapi-Tenant': 'diku',
+                },
+            })
+            // get a token then navigate to the TabNavigater with params
+            .then((response) => {
+                const tokenName = 'x-okapi-token';
+                const okapi_token = response.headers[tokenName];
+                const {
+                    navigate
+                } = this.props.navigation;
+                navigate('MainTab', {
+                    isLoggedIn: true,
+                    token: okapi_token,
+                    username: this.userName,
+                    resources: resources,
+                    languages: languages,
+                });
+            })
+            .catch((error) => {
+                Alert.alert(`${error}`);
+            });
+    };
+
+    // if press forget password, show a message
+    _handleforgetpassword = () => {
+        Alert.alert('Please contact the library staff to help you reset password.');
+    }
+
+
+  render() {
+    return (
+      <KeyboardAwareScrollView style={LoginStyles.loginview}>
+        <View style={{ flexDirection: 'row',
+          height: 150,
+          marginTop: 60,
+          justifyContent: 'center',
+          alignItems: 'flex-start' }}
+        >
+        <Image
+          style={{ maxHeight: 210, maxWidth: 210 }}
+          source={require('./image/login.png')}
+        />
+        </View>
+          <View style={{ marginTop: 80 }}>
+            <View style={LoginStyles.TextInputView}>
+              <TextInput
+                style={LoginStyles.TextInput}
+                placeholder="Username"
+                underlineColorAndroid={'transparent'}
+                onChangeText={(text) => {
+                  this.userName = text;
+                }}
+              />
+            </View>
+            <View style={LoginStyles.TextInputView}>
+              <TextInput
+                style={LoginStyles.TextInput}
+                placeholder="Password"
+                secureTextEntry
+                underlineColorAndroid={'transparent'}
+                onChangeText={(text) => {
+                  this.password = text;
+                }}
+              />
+            </View>
+          <LoginButton name="Log in" onPressCallback={this.onPressCallback} />
+          <TouchableOpacity onPress={this._handleforgetpassword} >
+            <Text style={{ color: '#4A90E2', textAlign: 'right', marginTop: 10 }} >Forget password？</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAwareScrollView>
+    );
+  }
+}
+const LoginStyles = StyleSheet.create({
+  loginview: {
+    flex: 1,
+    padding: 30,
+    backgroundColor: '#ffffff',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  TextInputView: {
+    marginTop: 10,
+    height: 50,
+    backgroundColor: '#ffffff',
+    borderRadius: 5,
+    borderWidth: 0.3,
+    borderColor: '#000000',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+
+  TextInput: {
+    backgroundColor: '#ffffff',
+    height: 45,
+    margin: 18,
+  },
+});
